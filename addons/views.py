e23772d6f96e72e845b74ba1b14757ca0501e676
stebanos <stefan.gabriels@gmail.com>
2011-02-28 16:32:14 +0100

    Added some code that parses a Java manifest file (manifest.mf)

--- addons/views.py
+++ addons/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+def parse_manifest_file(path):
+    f = open(path)
+    lines = []
+    for line in f.readlines():
+        line = line.rstrip()
+        if line:
+            if line.startswith(' '):
+                lines[-1] += line[1:]
+            else:
+                lines.append(line)
+    f.close()
+    data = {}
+    for line in lines:
+        entry = line.split(':')
+        key = entry[0].strip()
+        value = None
+        if len(entry) > 2:
+            value = ':'.join(entry[1:]).strip()
+        elif len(entry) == 2:
+            value = entry[1].strip()
+        data[key] = value
+    return data
